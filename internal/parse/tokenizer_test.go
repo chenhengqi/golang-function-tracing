72ea4f43a5d5ad510e5cb0ce44250f11647d3678
chenhengqi <chenhengqi@outlook.com>
2021-01-04 08:39:02 +0800

    rename variable

--- internal/parse/tokenizer_test.go
+++ internal/parse/tokenizer_test.go
@@ -6,8 +6,8 @@ import (
 
 func TestTokenizer(t *testing.T) {
 	tokenizer := NewTokenizer([]byte(`(int, string, *struct{int}, []byte)`))
-	for token, raw, err := tokenizer.Next(); err == nil; {
-		t.Logf("%v: %s\n", token, raw)
-		token, raw, err = tokenizer.Next()
+	for token, literal, err := tokenizer.Next(); err == nil; {
+		t.Logf("%v: %s\n", token, literal)
+		token, literal, err = tokenizer.Next()
 	}
 }
