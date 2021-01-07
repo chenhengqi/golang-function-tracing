1e2ae692975116324c226581ea184df207f95c06
chenhengqi <chenhengqi@outlook.com>
2021-01-07 09:04:28 +0800

    add Parser stub

--- internal/parse/parser.go
+++ internal/parse/parser.go
@@ -2,7 +2,27 @@ package parse
 
 // Parser is used to parse function arguments
 type Parser struct {
-	tokenizer Tokenizer
+	tokenizer *Tokenizer
 	token     Token
 	literal   string
+}
+
+// NewParser creates a new parser
+func NewParser(src []byte) *Parser {
+	return &Parser{
+		tokenizer: NewTokenizer(src),
+	}
+}
+
+// Parse parses function arguments
+func (p *Parser) Parse() {
+
+}
+
+func (p *Parser) parseArgs() {
+
+}
+
+func (p *Parser) parseArg() {
+
 }
