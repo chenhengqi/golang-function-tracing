1033b4767481f66c38df56482903529d802c06fb
chenhengqi <chenhengqi@outlook.com>
2021-01-08 09:09:46 +0800

    update Parser

--- internal/parse/parser.go
+++ internal/parse/parser.go
@@ -1,5 +1,10 @@
 package parse
 
+import (
+	"fmt"
+	"io"
+)
+
 // Parser is used to parse function arguments
 type Parser struct {
 	tokenizer *Tokenizer
@@ -16,7 +21,30 @@ func NewParser(src []byte) *Parser {
 
 // Parse parses function arguments
 func (p *Parser) Parse() {
+	for {
+		p.next()
+		switch p.token {
+		case None:
+			return
+		case LeftParenthesis:
+			p.parseArgs()
+		default:
+			panic(fmt.Sprintf("unexpected token: %+v, literal: %s", p.token, p.literal))
+		}
+	}
+}
 
+func (p *Parser) next() {
+	token, literal, err := p.tokenizer.Next()
+	if err != nil {
+		if err == io.EOF {
+			p.token = None
+			return
+		}
+		panic(err)
+	}
+	p.token = token
+	p.literal = literal
 }
 
 func (p *Parser) parseArgs() {
