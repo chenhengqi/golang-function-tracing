e3f3215ee734ec4688557fb871c8131175123f6f
chenhengqi <chenhengqi@outlook.com>
2021-01-16 10:31:53 +0800

    update string inspect func

--- internal/codegen/string.go
+++ internal/codegen/string.go
@@ -3,18 +3,18 @@ package codegen
 const stringDef = `
 struct string {
     u64 len;
-    char data[24];
+    u8 data[24];
 };
 `
 
-const stringInspect = `
-// str should be initialized
+const stringInspectFunc = `
+// str should be zero-initialized
 void string_inspect(u64 *base_addr, struct string *str) {
     // string length
-    bpf_probe_read(&str.len, sizeof(str.len), base_addr + 2);
+    bpf_probe_read(&str.len, sizeof(str.len), base_addr + 1);
     // string content
     u64 addr = 0;
-    bpf_probe_read(&addr, sizeof(addr), base_addr + 1);
+    bpf_probe_read(&addr, sizeof(addr), base_addr);
     bpf_probe_read(str.data, sizeof(str.data), (u64*)addr);
 }
 `
