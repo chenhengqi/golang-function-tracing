3ce69bbbed1ac7118b509c63f1c2590d77f57928
chenhengqi <chenhengqi@outlook.com>
2021-01-18 08:39:55 +0800

    remove comment

--- internal/codegen/ptr.go
+++ internal/codegen/ptr.go
@@ -1,7 +1,6 @@
 package codegen
 
 const ptrInspectFunc = `
-// str should be zero-initialized
 void ptr_inspect(u64 *base_addr, u64 *ptr_val) {
     bpf_probe_read(ptr_val, sizeof(u64), base_addr);
 }
