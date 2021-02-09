7fbe67496d7cfaceb961117957121328d87b88b3
chenhengqi <chenhengqi@outlook.com>
2021-02-09 16:34:20 +0800

    update Args impl

--- internal/codegen/codegen.go
+++ internal/codegen/codegen.go
@@ -1,13 +1,32 @@
 package codegen
 
 import (
+	"fmt"
 	"regexp"
 )
 
-var funcNamePattern = regexp.MustCompile(`(?P<package_name>.*)\.(?P<func_name>[a-zA-Z][a-zA-Z0-9]*)`)
-var funcNameWithReceiverPattern = regexp.MustCompile(`(?P<package_name>.*)\.(?P<receiver>\(?\*?[a-zA-Z][a-zA-Z0-9]*\)?)(?P<func_name>\.[a-zA-Z][a-zA-Z0-9]*)`)
+var funcNamePattern = regexp.MustCompile(`^(?P<package_name>.*)\.(?P<func_name>[a-zA-Z][a-zA-Z0-9]*)$`)
+var funcNameWithReceiverPattern = regexp.MustCompile(`^(?P<package_name>.*)\.(?P<receiver>\(?\*?[a-zA-Z][a-zA-Z0-9]*\)?)(?P<func_name>\.[a-zA-Z][a-zA-Z0-9]*)$`)
+
+var errWrongFunctionName = fmt.Errorf("wrong full qualified function name")
+var errReceiverShouldBePointer = fmt.Errorf("receiver should be pointer")
 
 // Args generates a code snippet for the provided arguments
-func Args(funcName, funcArgs string) (string, error) {
+func Args(fullFuncName, funcArgs string) (string, error) {
+	var receiver string
+
+	names := funcNamePattern.FindStringSubmatch(fullFuncName)
+	if len(names) != 3 {
+		names = funcNameWithReceiverPattern.FindStringSubmatch(fullFuncName)
+		if len(names) != 4 {
+			return "", errWrongFunctionName
+		}
+		receiver = names[2]
+	}
+
+	if len(receiver) < 2 || receiver[0] != '(' || receiver[1] != '*' {
+		return "", errReceiverShouldBePointer
+	}
+
 	return "", nil
 }
