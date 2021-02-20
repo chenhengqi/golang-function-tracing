b11f653b31db3c9f042561e0c5684784e3f8fc83
chenhengqi <chenhengqi@outlook.com>
2021-02-20 09:02:25 +0800

    add test

--- internal/codegen/tmpl_test.go
+++ internal/codegen/tmpl_test.go
@@ -5,3 +5,22 @@ import "testing"
 func TestPreamble(t *testing.T) {
 	t.Log(preamble())
 }
+
+func TestCode(t *testing.T) {
+	fields := []string{
+		"u64 x1;",
+		"char x2[128];",
+	}
+	snippets := []string{
+		`
+    u64* sp = (u64*)ctx->sp;
+    // string length
+    bpf_probe_read(&ev.len, sizeof(ev.len), sp + 2);
+    // string content
+    u64 addr = 0;
+    bpf_probe_read(&addr, sizeof(addr), sp + 1);
+    bpf_probe_read(ev.data, sizeof(ev.data), (u64*)addr);
+        `,
+	}
+	t.Log(code(fields, snippets))
+}
